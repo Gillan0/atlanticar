b3c2c3565787bdd2fc2363b1b2e7b5b19f60deff
GILLARD Antonino <antonino.gillard@imt-atlantique.net>
2024-03-27 10:50:42 +0100

    frontend update

---
+++ src/frontend/components/OfferItem.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import { Platform, UIManager, Pressable, View, Text, Button, LayoutAnimation, StyleSheet, Alert } from 'react-native';
+
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+const OfferItem = props => {  
+  const [display, setDisplay] = useState(false);
+
+  function toggle() {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+    setDisplay(!display);
+  };
+
+  function toCandidate() {
+    const dataToSend = {
+      username: 'user1',
+      password: 'pwd1234',
+      msg : "Je candidate",
+      offer : props.offer,
+    };
+    
+    // Options de la requête
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+    };
+    
+    // URL de l'endpoint où envoyer les données
+    const url = 'http://192.168.43.48:3000';
+    
+    // Envoi de la requête avec fetch
+    fetch(url, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la requête.');
+          Alert.alert("Erreur de connexion", "Vérifier que l'état de la connexion")
+        } else {
+          Alert.alert("Candidature envoyée", "Le conducteur n'a plus qu'à y répondre")
+        }
+        return response.json(); // Renvoie les données JSON de la réponse
+      })
+      .catch(error => {
+        console.error('Erreur :', error);
+      });
+    
+  }
+  
+
+  const displayColor = display ? "#11c9ef" : "#00b8de";
+
+  return (
+    <View style = {styles.mainContainer}>
+      <Pressable onPress = {toggle}>
+        <View style = {{...styles.titleContainer, backgroundColor : displayColor}}>
+          <View style = {{flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.defaultText}>Par {props.offer.user}</Text>
+            <Text style = {styles.defaultText}>{props.offer.cost} €</Text>
+          </View>
+          <View style = {{flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.destinations}>De {props.offer.start}</Text>
+            <Text style = {styles.destinations}>A {props.offer.end}</Text>
+          </View>
+          <View style = {{ flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.defaultText}>Le {props.offer.date} à {props.offer.time}</Text>
+            <Text style = {styles.defaultText}>{props.offer.places} place(s) restante(s)</Text>
+          </View>
+        </View>
+        </Pressable>
+        {display && (
+        <View style = {styles.revealContainer}>  
+          <View style = {styles.commentContainer}>
+            <Text >Commentaire :</Text>
+            <Text >{props.offer.comment}</Text>
+          </View>
+          <Button title={"Candidater"} color = {"#ddb500"} onPress={toCandidate}/>
+        </View>
+        )
+        }
+    </View>
+  )
+};
+const styles = StyleSheet.create({
+  destinations : {
+    fontSize :  20,
+    padding : 5,
+    color : "#fff",
+    fontWeight : "bold"
+  },
+  defaultText : {
+    fontSize :  14,
+    padding : 5,
+    color : "#fff"
+  },
+  mainContainer : {
+    backgroundColor : "#fff",
+    color : "#111111",
+    borderRadius : 10,
+    margin : 10,
+    shadowColor: "#000",
+    elevation: 5,
+  },
+  titleContainer : {
+    backgroundColor : "#99cc33",
+    borderRadius : 10,
+    padding : 10,
+    fontSize : 20
+  },
+  commentContainer : {
+    padding : 10,
+  },
+  revealContainer : {
+    padding : 10,
+    alignContent : "center"
+  }
+})
+
+export default OfferItem;
---
+++ src/frontend/screens/ConnectionScreen.js
@@ -0,0 +1,16 @@
+import {View, StyleSheet, Text, Pressable, ScrollView, StatusBar, Button, Alert, TextInput, FlatList} from "react-native";
+import React, {useState} from 'react';
+
+
+
+
+export default function ConnectionScreen() {
+
+
+    return (
+        <View>
+            <Text>Ecran de connexion</Text>
+        </View>
+    )
+
+}
