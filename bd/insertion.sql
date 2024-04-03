9b84ec13e7243318a7288948cae0d9e85de32298
napoknot21 <napoknot21@protonmail.com>
2024-04-03 18:29:51 +0200

    dabase updates

--- bd/insertion.sql
+++ bd/insertion.sql
@@ -1,1 +1,15 @@
-insert into requete values (DEFAULT, 'IMT Atlantique Brest', 'Carrefour Plouzané', '2024-08-01','10h30',0,'ne se prononce pas','JC')
+INSERT INTO request (author_id, departure_place, arrival_place, date, time, price, comment) 
+VALUES 
+(DEFAULT, 'IMT Atlantique Brest', 'Carrefour Plouzané', '2024-08-01', '10:30', 0, 'Does not express');
+
+INSERT INTO request (author_id, departure_place, arrival_place, date, time, price, comment) 
+VALUES 
+(DEFAULT, 'Gare de Brest', 'Lydl Plouzané', '2024-09-15', '14:45', 0, 'No comment');
+
+INSERT INTO request (author_id, departure_place, arrival_place, date, time, price, comment) 
+VALUES 
+(DEFAULT, 'Fnac Plouzané', 'Culture Brest', '2024-10-20', '16:20', 0, 'No comment');
+
+INSERT INTO request (author_id, departure_place, arrival_place, date, time, price, comment) 
+VALUES 
+(DEFAULT, 'KFC Brest', 'IMT Atlantique Brest', '2024-11-10', '12:00', 0, 'No comment');
