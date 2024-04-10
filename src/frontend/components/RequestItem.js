7b556816cce6f2a074bed22c11ba689c48463ade
Gillan0 <antonino.gillard@orange.fr>
2024-04-10 10:27:03 +0200

    updated url management and request item

---
+++ src/frontend/components/RequestItem.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { Platform, UIManager, Pressable, View, Text, Button, LayoutAnimation, StyleSheet, Alert } from 'react-native';
+import url from "./url.js"
+
+
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+const RequestItem = props => {  
+  const [display, setDisplay] = useState(false);
+
+  function toggle() {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+    setDisplay(!display);
+  };
+
+  function toCandidate() {
+    const dataToSend = {
+      username: 'user1',
+      password: 'pwd1234',
+      msg : "Je candidate à une requête",
+      request : props.request,
+    };
+    
+    // Options de la requête
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+    };
+    
+    // Envoi de la requête avec fetch
+    fetch(url, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erreur lors de la requête.');
+          Alert.alert("Erreur de connexion", "Vérifier que l'état de la connexion")
+        } else {
+          Alert.alert("Candidature envoyée", "Le conducteur n'a plus qu'à y répondre")
+        }
+        return response.json(); // Renvoie les données JSON de la réponse
+      })
+      .catch(error => {
+        console.error('Erreur :', error);
+      });
+    
+  }
+  
+
+  const displayColor = display ? "#11c9ef" : "#00b8de";
+
+  return (
+    <View style = {styles.mainContainer}>
+      <Pressable onPress = {toggle}>
+        <View style = {{...styles.titleContainer, backgroundColor : displayColor}}>
+          <View style = {{flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.defaultText}>Par {props.request.user}</Text>
+            <Text style = {styles.defaultText}>{props.request.cost} €</Text>
+          </View>
+          <View style = {{flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.destinations}>De {props.request.start}</Text>
+            <Text style = {styles.destinations}>A {props.request.end}</Text>
+          </View>
+          <View style = {{ flexDirection : "row", justifyContent : "space-between"}}>
+            <Text style = {styles.defaultText}>Le {props.request.date} à {props.request.time}</Text>
+          </View>
+        </View>
+        </Pressable>
+        {display && (
+        <View style = {styles.revealContainer}>  
+          <View style = {styles.commentContainer}>
+            <Text >Commentaire :</Text>
+            <Text >{props.request.comment}</Text>
+          </View>
+          <Button title={"Candidater"} color = {"#ddb500"} onPress={toCandidate}/>
+        </View>
+        )
+        }
+    </View>
+  )
+};
+const styles = StyleSheet.create({
+  destinations : {
+    fontSize :  20,
+    padding : 5,
+    color : "#fff",
+    fontWeight : "bold"
+  },
+  defaultText : {
+    fontSize :  14,
+    padding : 5,
+    color : "#fff"
+  },
+  mainContainer : {
+    backgroundColor : "#fff",
+    color : "#111111",
+    borderRadius : 10,
+    margin : 10,
+    shadowColor: "#000",
+    elevation: 5,
+  },
+  titleContainer : {
+    backgroundColor : "#99cc33",
+    borderRadius : 10,
+    padding : 10,
+    fontSize : 20
+  },
+  commentContainer : {
+    padding : 10,
+  },
+  revealContainer : {
+    padding : 10,
+    alignContent : "center"
+  }
+})
+
+export default RequestItem;
---
+++ src/frontend/components/request.js
@@ -0,0 +1,37 @@
+import url from "./url";
+
+export default function request(user,pwd,msg) {
+    // Données à envoyer
+  const dataToSend = {
+    username: user,
+    password: pwd,
+    message : msg
+  };
+  
+  // Options de la requête
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+  };
+  // Envoi de la requête avec fetch
+  return (
+  fetch(url, requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur lors de la requête.');
+      }
+      return response.json(); // Renvoie les données JSON de la réponse
+    })
+    .then(data => {
+      console.log('Réponse du serveur :', data.msg);
+      return data.offers;
+    })
+    .catch(error => {
+      console.error('Erreur :', error);
+    })  
+    )
+}
+
