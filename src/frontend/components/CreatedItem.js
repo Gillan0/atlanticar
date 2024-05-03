fc2cb8b500cebe8ed984b2b3b81e80066616bdb2
Gillan0 <antonino.gillard@orange.fr>
2024-05-03 11:31:53 +0200

    Changed UI to accept candidates

--- src/frontend/components/CreatedItem.js
+++ src/frontend/components/CreatedItem.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, Pressable, Image, StyleSheet, Alert } from 'react-native';
+import url from './url';
 
 export default function CreatedItem(props) {
-  // Fonction d'acceptation ou de refus d'une candidature
-  function accept(bool, id_candidate) {
+  const [candidates, setCandidates] = useState(props.candidates);
+  const [passengers, setPassengers] = useState(props.passengers);
+  const [conductor, setConductor] = useState(props.content.conductor);
+
+  function accept(bool, value_candidate, key_candidate) {
+    const [id, name] = value_candidate.split(':');
+    const id_candidate = parseInt(id, 10);
+
     const dataToSend = {
       id: props.id,
       password: props.password,
@@ -12,7 +19,7 @@ export default function CreatedItem(props) {
     };
 
     // Envoi de la requête avec fetch
-    fetch('url', {
+    fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(dataToSend),
@@ -24,12 +31,41 @@ export default function CreatedItem(props) {
         }
         return response.json();
       })
-      .catch((error) => console.error('Erreur :', error));
+      .then((data) => {
+        if (data[0].affectedRows) {
+          Alert.alert(bool ? "Candidature validée !" : "Candidature refusée !", "Le candidat en a été informé !")
+          let newCandidates = [...candidates];
+          newCandidates.splice(key_candidate,1)
+          setCandidates(newCandidates);
+          if (props.content.type == "offer") {
+            setPassengers([...passengers, value_candidate])
+            
+          } else if (props.content.type == "request") {
+            setConductor(value_candidate)
+          }
+        } else {
+          Alert.alert("Error", "No affected Rows")
+        }
+      })
+      .catch((error) => 
+        console.error('Erreur :', error)
+      );
   }
 
   return (
     <View style={styles.mainContainer}>
       <View style={{ ...styles.titleContainer }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={{...styles.defaultText, fontWeight : 'bold'}}>PARAMETRES </Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Pressable>
+              <Image source={require('../assets/parameters.png')} style={{ width: 30, height: 30, marginRight: 15 }} />
+            </Pressable>
+            <Pressable>
+              <Image source={require('../assets/bin.png')} style={{ width: 30, height: 30, marginLeft: 15 }} />
+            </Pressable>
+          </View>
+        </View>
         <View style={{ flexDirection: 'row', flex: 1 }}>
           <View style={{ flexDirection: 'column' }}>
             <View style={{ alignSelf: 'center', borderWidth: 4, borderColor: 'black', borderRadius: 10, width: 20, height: 20 }} />
@@ -37,24 +73,25 @@ export default function CreatedItem(props) {
             <View style={{ alignSelf: 'center', borderWidth: 4, borderColor: 'black', borderRadius: 10, width: 20, height: 20 }} />
           </View>
           <View style={{ flexDirection: 'column', flex: 1 }}>
+           
             <Text style={styles.destinations}>De {props.content.departure}</Text>
             <View>
               <Text style={styles.defaultText}>Le {props.content.date.substring(8, 10)}/{props.content.date.substring(5, 7)}/{props.content.date.substring(2, 4)} à {props.content.date.substring(11, 13)}h{props.content.date.substring(14, 16)}</Text>
               <View style={styles.revealContainer}>
-                {props.candidates && props.candidates.length > 0 ? (
+                {candidates && candidates.length > 0 ? (
                   <>
                     <Text style={{ ...styles.defaultText, fontWeight: 'bold' }}>Candidatures :</Text>
-                    {Object.values(props.candidates).map((value, key) => {
+                    {Object.values(candidates).map((value, key) => {
                       if (value) {
-                        const [id, name] = value.split(':'); // Assurez-vous que `value` n'est pas `undefined`
+                        const [id, name] = value.split(':');
                         return (
                           <View key={key} style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                             <Text style={styles.defaultText}>{name}</Text>
                             <View style={{ flexDirection: 'row' }}>
-                              <Pressable onPress={() => accept(true, parseInt(id, 10))}>
+                              <Pressable onPress={() => accept(true, value, key)}>
                                 <Image source={require('../assets/checkmark.png')} style={{ width: 30, height: 30, marginRight: 15 }} />
                               </Pressable>
-                              <Pressable onPress={() => accept(false, parseInt(id, 10))}>
+                              <Pressable onPress={() => accept(false, value, key)}>
                                 <Image source={require('../assets/cross.png')} style={{ width: 30, height: 30, marginLeft: 15 }} />
                               </Pressable>
                             </View>
@@ -66,10 +103,10 @@ export default function CreatedItem(props) {
                 ) : (<Text style={{ ...styles.defaultText }}>Aucune candidature pour le moment.</Text> )
                 }
 
-                {props.passengers && props.passengers.length > 0 ? (
+                {passengers && passengers.length > 0 ? (
                   <>
                     <Text style={{ ...styles.defaultText, fontWeight: 'bold' }}>Passagers déjà validés :</Text>
-                    {Object.values(props.passengers).map((value, key) => {
+                    {Object.values(passengers).map((value, key) => {
                       if (value) {
                         const [id, name] = value.split(':'); // Assurez-vous que `value` n'est pas `undefined`
                         return (
@@ -88,11 +125,11 @@ export default function CreatedItem(props) {
                 ) : props.content.type == "offer" && (
                   <Text style={{ ...styles.defaultText }}>Aucun passager validé pour le moment.</Text> 
                 )}
-                {props.content.conductor  && (
+                {conductor  && (
                           <>
                             <Text style = {{...styles.defaultText, fontWeight : 'bold'}}>Conducteur : </Text>
                             <View style = {{flexDirection : "row", justifyContent : "space-between"}}>                
-                                <Text style = {styles.defaultText}>{props.content.conductor.split(':')[1]}</Text>
+                                <Text style = {styles.defaultText}>{conductor.split(':')[1]}</Text>
                                 <View style = {{flexDirection : "row"}}>    
                                     <Pressable>
                                       <Image source = {require("../assets/cross.png")} style = {{width : 30, height : 30, marginLeft: 15}}/>
