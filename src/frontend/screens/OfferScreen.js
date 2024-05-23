d589c33f30d36626ee28f23ba746e1c6292fd95d
Gillan0 <antonino.gillard@orange.fr>
2024-05-23 04:35:14 +0200

    Fixed filters

--- src/frontend/screens/OfferScreen.js
+++ src/frontend/screens/OfferScreen.js
@@ -6,22 +6,24 @@ import SearchItem from "../components/SearchItem.js";
 import url from "../components/url.js";
 import { useNavigation } from '@react-navigation/native';
 import endScrollReached from "../components/endScrollReached.js";
+import isArrayEqual from "../checkFunctions/isArrayEqual.js";
 
 export default function OfferScreen({route}) { 
   const navigation = useNavigation();
   const [page, setPage] = useState(0);
-  const [lastCommand, setLastCommand] = useState("");
+  const [lastCommand, setLastCommand] = useState("get_default_offers");
+  const [lastParams, setLastParams] = useState(['','','','9999']);
   const [shownOffers,setShownOffers] = useState([]);
 
-  function request(command, newPage, parameters=['','','','9999']) {
+  function request(command, newPage, parameters=['','','','9999'], reset=false) {
+    console.log(lastParams)
     // Données à envoyer
     const dataToSend = {
       id: route.params.id,
       password: route.params.password,
       command : command,
-      parameters : [...parameters, shownOffers.length % 20 == 0 ? newPage : page]
+      parameters : [...parameters, reset ? 0 : (shownOffers.length % 20 == 0 ? newPage : page)]
     };
-    setLastCommand(command)
     // Options de la requête
     const requestOptions = {
       method: 'POST',
@@ -30,7 +32,6 @@ export default function OfferScreen({route}) {
       },
       body: JSON.stringify(dataToSend) // Convertir les données en format JSON
     };
-  
     // Envoi de la requête avec fetch
     fetch(url, requestOptions)
       .then(response => {
@@ -41,12 +42,27 @@ export default function OfferScreen({route}) {
       })
       .then(data => {
         if (data[0].length > 0) {
+          
           if (shownOffers.length % 20 == 0) {
-            setPage(newPage)
-            setShownOffers([...shownOffers, ...data[0]]);
+            if (lastCommand === command && isArrayEqual(lastParams,parameters) && !reset){
+              setPage(newPage)
+              setShownOffers([...shownOffers, ...data[0]])
+            } else {
+              setPage(0)
+              setShownOffers(data[0])
+            }
           } else {
-            setShownOffers([...shownOffers.slice(0,- shownOffers.length % 20), ...data[0]]);
+            if (lastCommand === command && isArrayEqual(lastParams,parameters) && !reset){
+              setPage(newPage)
+              setShownOffers([...shownOffers.slice(0, - shownOffers.length % 20), ...data[0]]);
+            } else {
+              setPage(0)
+              setShownOffers(data[0])
+            }
           }
+          
+          setLastCommand(command)
+          setLastParams(parameters ? parameters : [])
         }
       })
       .catch(error => {
@@ -59,7 +75,7 @@ export default function OfferScreen({route}) {
       try {
         setPage(0)
         setShownOffers([])
-        request('get_default_offers', 0)
+        request('get_default_offers', 0, ['','','','9999'], true)
       } catch (error) {
         console.error(error)
       }
@@ -78,15 +94,17 @@ export default function OfferScreen({route}) {
         <ScrollView 
         onScrollBeginDrag={({nativeEvent}) => {
             if (endScrollReached(nativeEvent)) {
-              request("get_default_offers", page + 1)
+              request(lastCommand, page + 1, lastParams, false)
             }
           }}
           scrollEventThrottle={400}>
           <SearchItem request={request} type="offer"/>
-            {shownOffers.map((item) =>   <OfferItem key ={item.id}
+            { 
+              shownOffers.map((item, index) =>   <OfferItem key ={index}
                                       account = {route.params}
                                       offer = {item}
-                                      />)}
+                                      />)
+            } 
             <View style = {{backgroundColor : "white", flex : 1, padding : 50}}/>
           </ScrollView>
           <View style={{position:"absolute", 
@@ -95,9 +113,7 @@ export default function OfferScreen({route}) {
                     right : 10,
                     elevation : 5}}>
           <Pressable onPress = {() => navigation.navigate("CreateOffer")}>
- 
-              <Image source = {require("../assets/plus-button.png")} style={{maxWidth : 60, maxHeight : 60}}/>    
-          
+              <Image source = {require("../assets/plus-button.png")} style={{maxWidth : 60, maxHeight : 60}}/>             
           </Pressable>
         </View>
     </View>
