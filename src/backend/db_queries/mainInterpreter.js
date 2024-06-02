a1a1c6e3a8a6a83c112c594b1f01e1493b816646
Charly Martin-Avila <napoknot21@protonmail.com>
2024-06-02 23:00:10 +0200

    Update mainInterpreter.js

--- src/backend/db_queries/mainInterpreter.js
+++ src/backend/db_queries/mainInterpreter.js
@@ -1,11 +1,34 @@
-const {accountInterpreter} = require("./account/accountInterpreter.js")
-const {announcementsInterpreter} = require("./announcements/announcementsInterpreter.js")
-const {applicationsInterpreter} = require("./applications/applicationsInterpreter.js")
+/**
+ * Import account interpreter module.
+ * @module accountInterpreter
+ */
+const { accountInterpreter } = require("./account/accountInterpreter.js");
 
+/**
+ * Import announcements interpreter module.
+ * @module announcementsInterpreter
+ */
+const { announcementsInterpreter } = require("./announcements/announcementsInterpreter.js");
+
+/**
+ * Import applications interpreter module.
+ * @module applicationsInterpreter
+ */
+const { applicationsInterpreter } = require("./applications/applicationsInterpreter.js");
+
+/**
+ * Combines all interpreters into a single interpreter object.
+ * @constant
+ * @type {Object}
+ */
 const interpreter = {
     ...accountInterpreter,
-    ...announcementsInterpreter, 
+    ...announcementsInterpreter,
     ...applicationsInterpreter
-}
+};
 
+/**
+ * Exports the combined interpreter object.
+ * @exports interpreter
+ */
 exports.interpreter = interpreter;
