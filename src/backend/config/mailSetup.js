0324f63db965f2240f8acb2e1a07b4b249616c41
Charly Martin-Avila <napoknot21@protonmail.com>
2024-06-03 00:27:22 +0200

    Update mailSetup.js

--- src/backend/config/mailSetup.js
+++ src/backend/config/mailSetup.js
@@ -1,28 +1,45 @@
 require('dotenv').config();
 const nodemailer = require("nodemailer");
 
-
+/**
+ * Creates a transporter object using the default SMTP transport.
+ * @constant
+ * @type {Object}
+ */
 const transporter = nodemailer.createTransport({
     service     : process.env.MAIL_SERVICE,
     host        : process.env.MAIL_HOST,
     port        : process.env.MAIL_PORT,
-    secure      : process.env.MAIL_SECURE,
-
+    secure      : process.env.MAIL_SECURE === 'true', // Convert string to boolean
     auth: {
         user      : process.env.MAIL_USER,
         pass      : process.env.MAIL_PASSWORD,
     },
-})
+});
 
 
+/**
+ * Sends an email using the specified transporter and mail options.
+ * @async
+ * @function sendMail
+ * @param {Object} transporter - The nodemailer transporter object.
+ * @param {Object} mailOptions - The mail options object containing sender, recipient, subject, and body of the email.
+ * @returns {Promise<void>} A promise that resolves when the email is sent.
+ */
 const sendMail = async (transporter, mailOptions) => {
     try {
         await transporter.sendMail(mailOptions);
-        console.log("Email has been sent !");
-    }  catch (error) {
-        console.error(error);
+        console.log("Email has been sent!");
+    } catch (error) {
+        console.error("Error sending email:", error);
     }
-}
+};
 
+
+/**
+ * Exports the transporter and sendMail function.
+ * @exports transporter
+ * @exports sendMail
+ */
 exports.transporter = transporter;
 exports.sendMail = sendMail;
