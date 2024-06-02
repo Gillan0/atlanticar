578bc8bafa92721f63c15d1a03ef6e43e69fa14f
Gillan0 <antonino.gillard@orange.fr>
2024-06-02 03:02:25 +0200

    Added comments

--- src/frontend/hubs/AnnouncementHub.js
+++ src/frontend/hubs/AnnouncementHub.js
@@ -5,108 +5,129 @@ import CreateAnnouncementScreen from "../screens/CreateAnnouncementScreen.js"
 import RequestScreen from '../screens/RequestScreen.js';
 import {View, StyleSheet, Text, Pressable, StatusBar, Alert, TextInput, Image, ScrollView} from "react-native";
 
-/* 
-Gray : #cbcbcb
-Light blue : #00b8de
-Dark blue : #0c2340
-Green : #99cc33
-*/
+// Navigation Type : Stack
 const Stack = createNativeStackNavigator()
 
+/**
+ * Allows navigation to the screens : 
+ *    -  listing all announcements not created / applied to by the user.
+ *    -  creating an annoucement
+ * 
+ * @param {Route<string>} route - Navigation route  
+ * @returns {React.ReactElement}
+ */
 export default function AnnouncementHub({route}) {
 
+  // Check announcement type (Offer / Request)
   if (route.params.type == "offer") {
     return (
-      <Stack.Navigator>
-        <Stack.Screen 
-        name = "OffersList" 
-        component={OfferScreen}
-        initialParams={route.params}
-        options = {{
-            title : "Offres",
-            headerStyle : {
-              backgroundColor : "#0c2340"
-            },
-            
-            headerTitleStyle : {
-              fontWeight : "bold",
-              color : "#fff"
-            },
-            headerRight: () => (
-             <Pressable onPress={() => Alert.alert('Principe des offres', '• Si vous possédez une voiture, vous pouvez créer une offre. \n • Si vous cherchez un conducteur pour votre destination, vous pouvez candidater !')}>
-              <Image
-                source={require('../assets/blue_info.png')}
-                style={{ width: 75, height: 35, marginRight: 45 }}
-                resizeMode="contain"
-              />
-            </Pressable>
-            )
-        }}
-        />
+      <>
+        {/* Stack navigation type */}
+        <Stack.Navigator>
+
+          {/* Adds option to go to the main Offers Screen*/}
+          <Stack.Screen 
+            name = "OffersList" 
+            component={OfferScreen}
+            initialParams={route.params}
+            options = {{
+                title : "Offres",
+                headerStyle : {
+                  backgroundColor : "#0c2340"
+                },
+                
+                headerTitleStyle : {
+                  fontWeight : "bold",
+                  color : "#fff"
+                },
+                // Help popup
+                headerRight: () => (
+                <Pressable onPress={() => Alert.alert('Principe des offres', '• Si vous possédez une voiture, vous pouvez créer une offre. \n • Si vous cherchez un conducteur pour votre destination, vous pouvez candidater !')}>
+                  <Image
+                    source={require('../assets/white_info.png')}
+                    style={{ width: 35, height: 35, marginRight: 5 }}
+                    resizeMode="contain"
+                  />
+                </Pressable>
+                )
+            }}
+          />
+
+          {/* Adds option to go to the CreateOffer Screen */}
           <Stack.Screen 
             name = "CreateOffer" 
             component={CreateAnnouncementScreen}
             initialParams={route.params}
             options = {{
-            title : "Créer une Offre",
-            headerStyle : {
-              backgroundColor : "#0c2340"
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle : {
-              fontWeight : "bold",
-              color : "#fff"
-            }
-          }}  
-        />
-      </Stack.Navigator>
-  )
+              title : "Créer une Offre",
+              headerStyle : {
+                backgroundColor : "#0c2340"
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle : {
+                fontWeight : "bold",
+                color : "#fff"
+              }
+            }}  
+          />
+        
+        </Stack.Navigator>
+      </>
+    )
   }
   else if (route.params.type == "request") {
     return (
-      <Stack.Navigator>
-        <Stack.Screen 
-        name = "RequestsList" 
-        component={RequestScreen}
-        initialParams={route.params}
-        options = {{
-            title : "Requêtes",
-            headerStyle : {
-              backgroundColor : "#0c2340"
-            },
-            
-            headerTitleStyle : {
-              fontWeight : "bold",
-              color : "#fff"
-            },
-            headerRight: () => (
-              <Pressable onPress={() => Alert.alert('Principe des reqêtes', '• Si vous chercher un voiture pour votre trajet, vous pouvez créer une requête. \n • Si vous cherchez des passagers pour votre destination, vous pouvez candidater !')}>
-               <Image
-                 source={require('../assets/blue_info.png')}
-                 style={{ width: 75, height: 35, marginRight: 45 }}
-                 resizeMode="contain"
-               />
-             </Pressable>
-             )
-        }}
-        />
-        <Stack.Screen 
-            name = "CreateRequest" 
-            component={CreateAnnouncementScreen}
-            initialParams={route.params}
-            options = {{
-            title : "Créer une Requête",
-            headerStyle : {
-              backgroundColor : "#0c2340"
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle : {
-              fontWeight : "bold",
-              color : "#fff"
-            }
-          }}  
-        />
-      </Stack.Navigator>
-  )
+      <>
+        {/* Stack navigation type */}
+        <Stack.Navigator>
+
+          {/* Adds option to go to the main Requests Screen */}
+          <Stack.Screen 
+          name = "RequestsList" 
+          component={RequestScreen}
+          initialParams={route.params}
+          options = {{
+              title : "Requêtes",
+              headerStyle : {
+                backgroundColor : "#0c2340"
+              },
+              
+              headerTitleStyle : {
+                fontWeight : "bold",
+                color : "#fff"
+              },
+              // Help popup
+              headerRight: () => (
+                <Pressable onPress={() => Alert.alert('Principe des reqêtes', '• Si vous chercher un voiture pour votre trajet, vous pouvez créer une requête. \n • Si vous cherchez des passagers pour votre destination, vous pouvez candidater !')}>
+                <Image
+                  source={require('../assets/white_info.png')}
+                  style={{ width: 40, height: 40, marginRight: 5 }}
+                  resizeMode="contain"
+                />
+              </Pressable>
+              )
+          }}
+          />
+
+          {/* Adds option to go to the CreateRequest Screen */}
+          <Stack.Screen 
+              name = "CreateRequest" 
+              component={CreateAnnouncementScreen}
+              initialParams={route.params}
+              options = {{
+              title : "Créer une Requête",
+              headerStyle : {
+                backgroundColor : "#0c2340"
+              },
+              headerTintColor: "#fff",
+              headerTitleStyle : {
+                fontWeight : "bold",
+                color : "#fff"
+              }
+            }}  
+          />
+        </Stack.Navigator>
+      </>
+    )
   }
 };
--- src/frontend/screens/SignUpScreen.js
+++ src/frontend/screens/SignUpScreen.js
@@ -2,17 +2,31 @@ import {View, StyleSheet, Text, Pressable, StatusBar, Alert, TextInput, Image, S
 import React, {useState, useRef} from 'react';
 import url from "../misc/url.js";
 import { useNavigation } from '@react-navigation/native';
-import {isIMTAdress} from '../checkFunctions/isIMTAdress.js'
-import {isOnlyNumbers} from '../checkFunctions/isOnlyNumbers.js'
+import isIMTAdress from '../checkFunctions/isIMTAdress.js'
+import isOnlyNumbers from '../checkFunctions/isOnlyNumbers.js'
 
 
-// Ecran de creation de compte
+/**
+ * Displays the screen where you can to sign up
+ * 
+ * @returns {React.ReactElement}
+ */
 export default function SignUpScreen(){
-
+    // Get navigation to allow navigation between screens
     const navigation = useNavigation();
+    
+    // Initialize variables for the prompts necessary to sign up
     const [prompts, setPrompts] = useState(['','', '']);
+    
+    // Allows to reference a ReactElement's parameters and change it
     const scrollContainer = useRef();
 
+    /**
+     * Changes a prompts' assigned variable. Called when the value of a Textinput is changed
+     * 
+     * @param {string} text - text of the changed prompt 
+     * @param {number} index -  index of the changed prompt in the prompts variable
+     */
     function changePrompts(text,index) {
         const newPrompts = [...prompts];
         newPrompts[index] = text.trim();
@@ -20,9 +34,14 @@ export default function SignUpScreen(){
         setPrompts(newPrompts);
     }
 
+    /**
+     * Sends a server request to sign up a new user to the database
+     * 
+     * @returns {null} 
+     */
     function SignUp() {
+        // Check for textual constraints in the different prompts
 
-        //Verification des contraintes textuelles 
         if (prompts[0].length > 15 || prompts[1].length > 15) {
             Alert.alert('Erreur !', 'Le nom d\'utilisateur et/ou le mot de passe ne peut pas dépasser 15 caractères.');
             return;
@@ -31,12 +50,10 @@ export default function SignUpScreen(){
             Alert.alert('Erreur !', 'Le numéro de téléphone ne peut pas dépasser 10 caractères.');
             return;
         }
-
         if (!prompts[0] || !prompts[1] || !prompts[2] || !prompts[3]) {
             Alert.alert('Erreur !', 'Veuillez remplir tous les champs.');
             return;
         }
-        
         if (!isOnlyNumbers(prompts[2])) {
             Alert.alert('Erreur !', 'Le numéro de téléphone doit contenir uniquement des chiffres.');
             return;
@@ -45,6 +62,8 @@ export default function SignUpScreen(){
             Alert.alert('Erreur !', 'L\'adresse mail doit être une adresse mail IMT.');
             return;
         }
+
+        // Define data to send to the server
         const dataToSend = {
             id: prompts[0],
             password: prompts[1].replace(/ /g, ''),
@@ -53,33 +72,40 @@ export default function SignUpScreen(){
             command : "signUp",
             parameters : [prompts[0], prompts[1].replace(/ /g, ''), prompts[2].match(/.{1,2}/g).join(' '), prompts[3]]
         };
-        //Options de la requête
+
+        // Options of the http request
         const requestOptions = {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+            body: JSON.stringify(dataToSend) // Convert data to JSON format
           };
         
-        // Envoi de la requête avec fetch
+        // Send http request
         fetch(url, requestOptions)
+        // Check if obtained response is correct
         .then(response => {
             if (!response.ok) {
                 Alert.alert("COMPTE NON CRÉE")
                 throw new Error('Erreur lors de la requête.');
             }
-            return response.json(); // Renvoie les données JSON de la réponse
+            // Returns the JSON data of the response
+            return response.json();
         })
+        // Exploit useable data
         .then(data => {
-            console.log(data)
+            // Changes screen if database modified
             if (data[0].affectedRows == 1) {
                 Alert.alert('Votre compte est bien crée !', 'Bienvenue sur Atlanticar !');
                 navigation.replace("Main", {id : data[0].insertId, username : prompts[0], password : prompts[1], phone_number : prompts[2].match(/.{1,2}/g).join(' '), email: prompts[3]})
             } else {
+                // Shows error message to user
                 Alert.alert('Erreur ! Le nom d\'utilisateur ou le numéro de téléphone existe déjà.');
             }
         })
+        
+        // Error failsafe
         .catch(error => {
             console.error('Erreur :', error);
         });  
@@ -88,47 +114,62 @@ export default function SignUpScreen(){
     return (
         
         <View style = {{flex : 1, backgroundColor : "white"}}>
-          <StatusBar backgroundColor="#99cc33"/>  
-          <ScrollView ref = {scrollContainer}>
-            <View style = {styles.formContainer}>
-                <Image source={require("../assets/logo.jpg")} style = {{height : 200, width : 300, alignSelf : "center"}}/>
-                <View>
-                    <Text style={styles.text}>Nom d'utilisateur</Text>
-                    <TextInput  style={styles.input}
-                                onChangeText={(text) => changePrompts(text, 0)}
-                                onPress={() => scrollContainer.current.scrollTo({y : 75,  animated : true})}
-                                />
+            {}
+            <StatusBar backgroundColor="#99cc33"/>  
+            <ScrollView ref = {scrollContainer}>
+                <View style = {styles.formContainer}>
+                    {/* App logo */}
+                    <Image source={require("../assets/logo.jpg")} style = {{height : 200, width : 300, alignSelf : "center"}}/>
+                    
+                    {/* Username prompt */}
+                    <View>
+                            <Text style={styles.text}>Nom d'utilisateur</Text>
+                            <TextInput  style={styles.input}
+                                    onChangeText={(text) => changePrompts(text, 0)}
+                                    onPress={() => scrollContainer.current.scrollTo({y : 75,  animated : true})}
+                                    />
+                    </View>
+
+                    {/* Password prompt */}
+                    <View>
+                        <Text style={styles.text}>Mot de passe</Text>
+                        <TextInput  style={styles.input}
+                                    secureTextEntry={true}
+                                    onChangeText={(text) => changePrompts(text, 1)}
+                                    onPress={() => scrollContainer.current.scrollTo({y : 125,  animated : true})}
+                                    />
+                    </View>
+
+                    {/* Phone number prompt */}
+                    <View>
+                        <Text style={styles.text}>Numéro de téléphone</Text>
+                        <TextInput  value = {prompts[2] ? prompts[2].match(/.{1,2}/g).join(' ') : ''}
+                                    style={styles.input}
+                                    onChangeText={(text) => changePrompts(text.replace(/\s/g, ''), 2)}
+                                    onPress={() => scrollContainer.current.scrollTo({y : 200,  animated : true})}
+                                    />
+                    </View>
+
+                    {/* Email address prompt */}
+                    <View style={{flex: 1}}>
+                        <Text style={styles.text}>Adresse mail IMT</Text>
+                        <TextInput  style={styles.input}
+                                    onChangeText={(text) => changePrompts(text, 3)}
+                                    onPress={() => scrollContainer.current.scrollTo({y : 300,  animated : true})}
+                                    />
+                    </View>
+
+                    {/* Create Account Button */}
+                    <View style = {styles.button}>
+                        <Pressable onPress = {()=> SignUp()}>
+                            <Text style = {styles.buttonText}> Créer mon compte </Text>
+                        </Pressable>
+                    </View>
+
                 </View>
-                <View>
-                    <Text style={styles.text}>Mot de passe</Text>
-                    <TextInput  style={styles.input}
-                                secureTextEntry={true}
-                                onChangeText={(text) => changePrompts(text, 1)}
-                                onPress={() => scrollContainer.current.scrollTo({y : 125,  animated : true})}
-                                />
-                </View>
-                <View>
-                    <Text style={styles.text}>Numéro de téléphone</Text>
-                    <TextInput  value = {prompts[2] ? prompts[2].match(/.{1,2}/g).join(' ') : ''}
-                                style={styles.input}
-                                onChangeText={(text) => changePrompts(text.replace(/\s/g, ''), 2)}
-                                onPress={() => scrollContainer.current.scrollTo({y : 200,  animated : true})}
-                                />
-                </View>
-                <View style={{flex: 1}}>
-                    <Text style={styles.text}>Adresse mail IMT</Text>
-                    <TextInput  style={styles.input}
-                                onChangeText={(text) => changePrompts(text, 3)}
-                                onPress={() => scrollContainer.current.scrollTo({y : 300,  animated : true})}
-                                />
-                </View>
-                <View style = {styles.button}>
-                    <Pressable onPress = {()=> SignUp()}>
-                        <Text style = {styles.buttonText}> Créer mon compte </Text>
-                    </Pressable>
-                </View>
-            </View>
-            <View style = {{flex: 1, height : 300}}/>
+
+                {/* White space to allow scrolling when keyboard shows up*/}
+                <View style = {{flex: 1, height : 300}}/>
             </ScrollView>
         </View>
     )
