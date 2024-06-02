fc73473e7b1d2c7562653a7fc0e5b645deb9f0e0
Gillan0 <antonino.gillard@orange.fr>
2024-06-03 01:16:55 +0200

    Comments added

--- src/frontend/screens/AnnouncementScreen.js
+++ src/frontend/screens/AnnouncementScreen.js
@@ -1,13 +1,31 @@
-import {View, StyleSheet, Text, Pressable, ScrollView, StatusBar} from "react-native";
+import {View, Text, ScrollView, StatusBar} from "react-native";
 import React, {useState} from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 import CreatedItem from "../components/CreatedItem.js";
 import url from "../misc/url.js";
 
+
+/**
+ * Displays the user's created announcements (offer or request)
+ * 
+ * @param {Route<string>} route -  Navigation route
+ * @returns 
+ */
 export default function AnnouncementScreen({route}) {
+
+  // Initializing variables to chosse which announcements to render
   const [shownAnnouncements,setShownAnnouncements] = useState({});
+
+  /**
+   * Sends a http request to the server and changes rendered 
+   * announcements based on answer
+   * 
+   * @param {String} command - command interpreted by server
+   * @param {Array<*>} parameters - parameters for the sql script 
+   */
   function request(command,parameters=[]) {
-    // Données à envoyer
+
+    // Data to send to server
     const dataToSend = {
       id: route.params.id,
       password: route.params.password,
@@ -15,54 +33,72 @@ export default function AnnouncementScreen({route}) {
       parameters : parameters
     };
   
-    // Options de la requête
+    // HTTP request options
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+      body: JSON.stringify(dataToSend) // Data converted to JSON format
     };
   
-    // Envoi de la requête avec fetch
+    // Sends HTTP request
     fetch(url, requestOptions)
+
+      // Checks if answer exploitable
       .then(response => {
         if (!response.ok) {
           throw new Error('Erreur lors de la requête.');
         }
-        return response.json(); // Renvoie les données JSON de la réponse
+        return response.json();
       })
+
+      // Changes rendered annoucements
       .then(data => {
         console.log(data[0]);
         setShownAnnouncements(data[0]);
       })
+
+      // Error failsafe
       .catch(error => {
         console.error('Erreur :', error);
       });  
     }
+ 
+  // Once Screen focused on
   useFocusEffect(
     React.useCallback(() => {
       console.log('AnnouncementScreen');
+      
       try {
+        
+        // Request command changes based on type of screen (offer / request)
         if (route.params.type == "offer") {
           request("get_announcements_offers")
         } else if (route.params.type == "request") {
           request("get_announcements_requests")
         }
         console.log(shownAnnouncements);
+
       } catch (error) {
+        // Error failsafe
         console.error(error)
       }
 
       return () => {
-        // Optionnel : nettoyer lorsqu'on quitte l'écran
+        // Once you quit the screen
       };
     }, [])
   );
+
+
   return (
       <View style = {{flex : 1, backgroundColor : "white"}}>
         <StatusBar backgroundColor="#99cc33"/>
+
         <ScrollView>
+
+          {/* Rendered announcements */}
           {shownAnnouncements.length > 0 ? 
             Object.values(shownAnnouncements).map( (value, index) => (
             <CreatedItem key = {index} 
@@ -83,6 +119,7 @@ export default function AnnouncementScreen({route}) {
                           />
           )) 
           : (
+            /* Message when no annoucement was created */ 
             route.params.type == "offer" ? 
               <Text style = {{alignSelf:"center", padding : 20, fontSize : 16}}> Vous n'avez créé aucune Offre </Text>
               : 
@@ -90,6 +127,7 @@ export default function AnnouncementScreen({route}) {
             )
           }
         </ScrollView>
+
       </View>
   )
 }
--- src/frontend/screens/ApplicationScreen.js
+++ src/frontend/screens/ApplicationScreen.js
@@ -4,10 +4,28 @@ import ApplicationItem from "../components/ApplicationItem";
 import { useFocusEffect } from '@react-navigation/native';
 import url from "../misc/url";
 
+
+/**
+ * Displays the user's applications (offer or request)
+ * 
+ * @param {Route<string>} route -  Navigation route
+ * @returns 
+ */
 export default function ApplicationScreen({route}) {
+
+  // Initializing variables to chosse which applications to render
   const [shownApplications,setShownApplications] = useState([]);
+
+  /**
+   * Sends a http request to the server and changes rendered 
+   * announcements based on answer
+   * 
+   * @param {String} command - command interpreted by server
+   * @param {Array<*>} parameters - parameters for the sql script 
+   */
   function request(command,parameters=[]) {
-    // Données à envoyer
+
+    // Data to send to server
     const dataToSend = {
       id: route.params.id,
       password: route.params.password,
@@ -15,35 +33,45 @@ export default function ApplicationScreen({route}) {
       parameters : parameters
     };
   
-    // Options de la requête
+    // HTTP request options
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(dataToSend) // Convertir les données en format JSON
+      body: JSON.stringify(dataToSend) // Data converted to JSON format
     };
   
-    // Envoi de la requête avec fetch
+    // Sends HTTP request
     fetch(url, requestOptions)
+
+      // Checks if answer exploitable
       .then(response => {
         if (!response.ok) {
           throw new Error('Erreur lors de la requête.');
         }
         return response.json(); // Renvoie les données JSON de la réponse
       })
+
+      // Changes rendered applications
       .then(data => {
         console.log(data);
         setShownApplications(data[0]);
       })
+
+      // Error failsafe
       .catch(error => {
         console.error('Erreur :', error);
       });  
     }
+
+  // Once Screen focused on
   useFocusEffect(
     React.useCallback(() => {
       console.log('Applicationscreen');
       try {
+
+        // Request command changes based on type of screen (offer / request)
         if (route.params.type == "offer") {
           request("get_applications_offers")
         } else if (route.params.type == "request") {
@@ -54,20 +82,34 @@ export default function ApplicationScreen({route}) {
       }
 
       return () => {
-        // Optionnel : nettoyer lorsqu'on quitte l'écran
+        // Optional : if you quit screen
       };
     }, [])
   );
 
-
-    return (
+  return (
       <View style = {{flex : 1, backgroundColor : "white"}}>
+        
         <StatusBar backgroundColor="#99cc33"/>
+        
         <ScrollView>
-        {shownApplications.map( (value, index) => (
-            <ApplicationItem key = {index} content = {{...value, type : route.params.type}} account = {route.params}/>
-          ))}
+
+          {/* Rendered applications */}
+          {shownApplications.length > 0 ? 
+            shownApplications.map( (value, index) => (
+                <ApplicationItem key = {index} content = {{...value, type : route.params.type}} account = {route.params}/>
+              ))
+            :(
+              /* Message when no application was created */ 
+              route.params.type == "offer" ? 
+                <Text style = {{alignSelf:"center", padding : 20, fontSize : 16}}> Vous n'avez candidaté à aucune Offre </Text>
+                : 
+                <Text style = {{alignSelf:"center", padding : 20, fontSize : 16}}> Vous n'avez candidaté à aucune Requête </Text>
+              )
+          }
+
         </ScrollView>
+      
       </View>
     )
 }
