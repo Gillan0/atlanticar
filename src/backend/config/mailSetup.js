982a28a60acf2245d66e817b24cd7f2e9148aad9
Gillan0 <antonino.gillard@orange.fr>
2024-06-02 02:19:20 +0200

    Added environment variable dependency

--- src/backend/config/mailSetup.js
+++ src/backend/config/mailSetup.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const nodemailer = require("nodemailer");
 
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // Use `true` for port 465, `false` for all other ports
+    service     : process.env.MAIL_SERVICE,
+    host        : process.env.MAIL_HOST,
+    port        : process.env.MAIL_PORT,
+    secure      : process.env.MAIL_SECURE,
+
     auth: {
-      user: "atlanticarservices@gmail.com",
-      pass: "nlkeyzfikaigmlkz",
+        user      : process.env.MAIL_USER,
+        pass      : process.env.MAIL_PASSWORD,
     },
 })
 
