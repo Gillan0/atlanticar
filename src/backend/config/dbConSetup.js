140173a12accd4de8f85416cd261fae6a8921fe0
Charly Martin-Avila <napoknot21@protonmail.com>
2024-06-03 00:23:45 +0200

    Update doc dbConSetup.js

--- src/backend/config/dbConSetup.js
+++ src/backend/config/dbConSetup.js
@@ -1,34 +1,50 @@
 const mysql = require('mysql');
 
-
+/**
+ * Establishes a connection to the MySQL database using environment variables.
+ * @constant
+ * @type {Object}
+ */
 const connection = mysql.createConnection({
-    host	 : process.env.DB_HOST,
-	port	 : process.env.DB_PORT,
-	user	 : process.env.DB_USER,
-	password : process.env.DB_PASSWORD,
-	database : process.env.DB_NAME
-})
+    host     : process.env.DB_HOST,
+    port     : process.env.DB_PORT,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
 
+
+/**
+ * Connects to the MySQL database and logs the connection status.
+ * @function
+ * @param {Error} err - The error object if the connection fails.
+ */
 connection.connect((err) => {
-    if (err) {
-      console.error('Erreur de connexion à la base de données :', err);
-      return;
-    }
-    console.log('Connecté à la base de données MySQL');
-})
+    err ? console.error('Error connecting to the database:', err) : console.log('Connected to the MySQL database');
+});
 
+
+/**
+ * Executes a SQL query and returns the results as a Promise.
+ * @function
+ * @param {string} sql - The SQL query string.
+ * @param {Array} params - The parameters for the SQL query.
+ * @returns {Promise} A promise that resolves with the query results or rejects with an error.
+ */
 const queryAsPromise = (sql, params) => {
-    console.log(sql, params)
+    console.log(sql, params);
     return new Promise((resolve, reject) => {
         connection.query(sql, params, (err, results) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(results);
-            }
-        })
+            err ? reject(err) : resolve(results);
+        });
     });
 };
 
+
+/**
+ * Exports the MySQL connection and queryAsPromise function.
+ * @exports connection
+ * @exports queryAsPromise
+ */
 exports.connection = connection;
 exports.queryAsPromise = queryAsPromise;
