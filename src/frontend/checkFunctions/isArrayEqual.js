a3207775027e73d9d6d4d9f91091f18fa65e0979
Charly Martin-Avila <napoknot21@protonmail.com>
2024-06-03 02:34:14 +0200

    Update isArrayEqual.js

--- src/frontend/checkFunctions/isArrayEqual.js
+++ src/frontend/checkFunctions/isArrayEqual.js
@@ -1,14 +1,19 @@
-function isArrayEqual(a,b) {
-    if (a.length !== b.length) {
-        return false;
-    }
+/**
+ * Compares two arrays for equality.
+ * 
+ * @param {Array} a - The first array to compare.
+ * @param {Array} b - The second array to compare.
+ * @returns {boolean} Returns true if the arrays are equal, false otherwise.
+ */
+function isArrayEqual(a, b) {
+    // Check if the arrays have the same length
+    if (a.length !== b.length) return false;
     
-    // Comparer chaque élément des deux tableaux
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            return false;
-        }
-    }
-    return true
+    // Compare each element of the two arrays
+    for (let i = 0; i < a.length; i++) {        
+        if (a[i] !== b[i]) return false;
+    }    
+    return true;
 }
+
 export default isArrayEqual;
