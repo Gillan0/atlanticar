6eca0366e71404777b85a4bb825d6143e3e2a113
Charly Martin-Avila <napoknot21@protonmail.com>
2024-06-03 02:50:53 +0200

    Update isOnlyNumbers.js

--- src/frontend/checkFunctions/isOnlyNumbers.js
+++ src/frontend/checkFunctions/isOnlyNumbers.js
@@ -1,11 +1,16 @@
 /**
- * Function to check if the input string contains only digits.
+ * Function to check if the input string is a valid price.
+ * An empty string is considered a valid price.
  * 
  * @param {String} str - The string to evaluate.
- * @returns {Boolean} - Indicates whether the string contains only digits.
+ * @returns {Boolean} - Indicates whether the string is a valid price.
  */
-function isOnlyNumbers(str) {
-    return /^\d+$/.test(str);
+function isValidPrice(str) {
+    if (str === '') {
+        return true;
+    }
+    const num = parseFloat(str);
+    return !isNaN(num);
 }
 
-export default isOnlyNumbers;
+export default isValidPrice;
