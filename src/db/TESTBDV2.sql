f4c617b85830f053af794a18c44a7dd7129ca39e
Gillan0 <antonino.gillard@orange.fr>
2024-06-04 12:15:39 +0200

    Changed email address of test accounts

--- src/db/TESTBDV2.sql
+++ src/db/TESTBDV2.sql
@@ -88,9 +88,9 @@ create table notification
 );
 
 #ajout de 6 profiles
-insert into account (user,password,phone_number, email) values ('JC','1234','06 12 34 56 78', 'jc.jean@gmail.com'),('Jean-Eude','1234','06 00 00 00 00','je.jeude@gmail.com'),
-('Eugène','1234','06 11 11 11 11', 'eugene.gene@gmail.com'),('Jean Michel IV', '2146','06 69 96 69 96', 'jeanmi.chel@gmail.com'),('Albert', 'alibaba','06 69 96 69 96', 'ali.baba@gmail.com'),
-('Tanya', 'Evil','06 66 66 66 66', 'ta.nya@gmail.com');
+insert into account (user,password,phone_number, email) values ('JC','1234','06 12 34 56 78', 'jc@imt-atlantique.net'),('Jean-Eude','1234','06 00 00 00 00','je@imt-atlantique.net'),
+('Eugène','1234','06 11 11 11 11', 'eugene@imt-atlantique.net'),('Jean Michel IV', '2146','06 69 96 69 96', 'jeanmi@imt-atlantique.net'),('Albert', 'alibaba','06 69 96 69 96', 'alibaba@imt-atlantique.net'),
+('Tanya', 'Evil','06 66 66 66 66', 'tanya@imt-atlantique.net');
 
 #41 requêtes
 insert into request values (DEFAULT, 'IMT Atlantique Brest','Carrefour Plouzané','2024-08-01 10:30:00',0,'',1,null);
